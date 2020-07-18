43ea0e4bb343a3127c47f64f2b025e38c5845233
forecho <caizhenghai@gmail.com>
2020-07-18 16:39:03 +0800

    fix: update ResponseHandler

--- src/ResponseHandler.php
+++ src/ResponseHandler.php
@@ -10,6 +10,7 @@ namespace yiier\helpers;
 class ResponseHandler
 {
     public $event;
+    public $successMessage;
 
     /**
      * 返回数据统一处理
@@ -20,22 +21,22 @@ class ResponseHandler
         if ($response->data !== null) {
             if (isset($response->data['code']) && isset($response->data['message'])) {
                 $response->data = [
-                    'code'    => $response->data['code'] ?: $response->statusCode,
-                    'data'    => isset($response->data['data']) ? $response->data['data'] : null,
+                    'code' => $response->data['code'] ?: $response->statusCode,
+                    'data' => isset($response->data['data']) ? $response->data['data'] : null,
                     'message' => $response->data['message'],
                 ];
             } elseif ($response->format != 'html' && !isset($response->data['message'])) {
                 $response->data = [
-                    'code'    => 0,
-                    'data'    => $response->data,
-                    'message' => '成功',
+                    'code' => 0,
+                    'data' => $response->data,
+                    'message' => $this->successMessage ?: \Yii::t('app', 'Success Message'),
                 ];
             } elseif (isset($response->data['message']) && $response->data['message'] != "" && !isset($response->data['code'])) {
                 $message = $response->data['message'];
                 unset($response->data['message']);
                 $response->data = [
-                    'code'    => 0,
-                    'data'    => isset($response->data[0]) ? $response->data[0] : $response->data,
+                    'code' => 0,
+                    'data' => isset($response->data[0]) ? $response->data[0] : $response->data,
                     'message' => $message,
                 ];
             }
